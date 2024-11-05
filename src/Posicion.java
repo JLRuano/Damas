9b4232af11b27718c2a2a02c455988096b748df5
JLRuano <j.l.ruano.clases@gmail.com>
2024-11-05 20:42:51 +0100

    paso 11 (constructor de dama)

--- src/Posicion.java
+++ src/Posicion.java
@@ -6,31 +6,46 @@ public class Posicion {
     private int fila;
     private char columna;
 
-    //paso 2 crear get y set
 
-    //get de fila
-    public int getFila() {
-        return fila;
+    //paso 3 constructor fila y columna
+
+    public Posicion (int fila, char columna) {
+            setFila(fila);
+            setColumna(columna);
     }
 
+
+
+    //paso 2 crear get y set
+
+
     //set de fila
-    public void setFila(int fila) {
+    private void setFila(int fila) {
         if (fila <1 || fila >8) {
             throw new IllegalArgumentException ("la fila tiene que estar entre 1 y 8");
         }
         this.fila = fila;
     }
 
-    //get de columna
-    public char getColumna() {
-        return columna;
+    //get de fila
+    public int getFila() {
+        return fila;
     }
 
+
     //set de columna
-    public void setColumna(char columna) {
+    private void setColumna(char columna) {
         if (columna < "a" || columna > "h") {
             throw new IllegalArgumentException ("la columna debe estar entre "a" y "h"");
         }
         this.columna = columna;
     }
+
+
+    //get de columna
+    public char getColumna() {
+        return columna;
+    }
+
+
 }
