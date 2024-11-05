47f19f9c581a30089a455cb12357a2d607063614
JLRuano <j.l.ruano.clases@gmail.com>
2024-11-05 20:36:30 +0100

    paso 10 (get y set de dama)

--- src/Posicion.java
+++ src/Posicion.java
@@ -2,7 +2,35 @@ package programacion.damas;
 
 public class Posicion {
 
+    //paso 1 crear atributos
     private int fila;
     private char columna;
 
+    //paso 2 crear get y set
+
+    //get de fila
+    public int getFila() {
+        return fila;
+    }
+
+    //set de fila
+    public void setFila(int fila) {
+        if (fila <1 || fila >8) {
+            throw new IllegalArgumentException ("la fila tiene que estar entre 1 y 8");
+        }
+        this.fila = fila;
+    }
+
+    //get de columna
+    public char getColumna() {
+        return columna;
+    }
+
+    //set de columna
+    public void setColumna(char columna) {
+        if (columna < "a" || columna > "h") {
+            throw new IllegalArgumentException ("la columna debe estar entre "a" y "h"");
+        }
+        this.columna = columna;
+    }
 }
