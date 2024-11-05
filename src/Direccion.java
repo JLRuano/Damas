4b84bf6f3c7d821d79d10cc56afb7d62ccded6a5
JLRuano <j.l.ruano.clases@gmail.com>
2024-11-05 20:09:04 +0100

    paso 4 (metodo toString)

--- src/Direccion.java
+++ src/Direccion.java
@@ -2,12 +2,23 @@ package programacion.damas;
 
 public enum Direccion {
 
+    //paso 1 crea un enumerado
     NORESTE ("noreste"), SURESTE ("sureste"), SUROESTE ("suroeste"), NOROESTE ("noroeste");
 
+    //paso 2 crea el atributo
     private String cadenaAMostrar;
 
+    //paso 3 crea el constructor
     private Direccion (String cadenaAMostrar)
     {
         this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+    //paso 4 crea el metodo
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Direccion{" +
+                "cadenaAMostrar='" + cadenaAMostrar + '\'' +
+                '}';
     }
 }
