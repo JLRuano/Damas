e6723b000ee4d32928d48956b038d899be7e562e
JLRuano <j.l.ruano.clases@gmail.com>
2024-11-10 18:53:05 +0100

    paso 14 (atributos de dama)

--- src/Dama.java
+++ src/Dama.java
@@ -1,5 +1,11 @@
 package programacion.damas;
 
 public class Dama {
+    //paso 1 crear atributos
+    private Color color;
+    private Posicion posicion;
+    private boolean esDamaEspecial;
+
+
 
 }
