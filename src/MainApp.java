eb902caf6e2bf8414151ada095921229149e9af2
JLRuano <j.l.ruano.clases@gmail.com>
2024-11-12 18:47:30 +0100

    paso 27 (crear metodo ejecutarOpcion para mainApp)

--- src/MainApp.java
+++ src/MainApp.java
@@ -5,5 +5,24 @@ public class MainApp {
     //paso 1 crea el atributo de clase dama
     private static Dama dama;
 
+    //paso 2 crea el metodo ejecutarOpcion
+    private static void ejecutarOpcion (int opcion) {
+        switch (opcion) {
+            case 1:
+                ;
+                break;
+            case 2:
+                ;
+                break;
+            case 3:
+                ;
+                break;
+            case 4:
+                ;
+                break;
 
+        }
+
+
+    }
 }
